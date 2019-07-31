ba6267b007b5a1548e7d083ce9e13f5cc8b06e57
joy <joy_yin@outlook.com>
2019-07-31 20:37:33 +0800

    add mirror tree

---
+++ Algorithms/mirrorTree.cpp
@@ -0,0 +1,24 @@
+#include <cstddef>
+
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+
+class Solution {
+public:
+    void Mirror(TreeNode *pRoot) {
+        if (pRoot) {
+            TreeNode *tmp = pRoot->left;
+            pRoot->left = pRoot->right;
+            pRoot->right = tmp;
+            Mirror(pRoot->left);
+            Mirror(pRoot->right);
+        }
+    }
+};
