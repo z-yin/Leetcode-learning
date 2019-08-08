112dbf711c168e7f51aa9bc4c7882800439591e7
zhaoyin666 <joy_yin@outlook.com>
2019-08-08 14:37:21 +0800

    add solution 2 of roman to integer

--- 13-roman-to-integer/cpp/roman-to-integer.cpp
+++ 13-roman-to-integer/cpp/roman-to-integer.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // 8 ms, 8.3 MB
 // switch here is faster than using unordered_map
-class Solution {
+class Solution1 {
 public:
     int romanCToInt (char c) {
         switch (c) {
@@ -32,6 +32,30 @@ public:
             if (i + 1 < s.size() && romanCToInt(s[i]) < romanCToInt(s[i + 1]))
                 ret += romanCToInt(s[i + 1]) - romanCToInt(s[i++]);
             else ret += romanCToInt(s[i]);
+        }
+        return ret;
+    }
+};
+
+
+// 8 ms, 8.3 MB
+class Solution2 {
+public:
+    int romanToInt(string s) {
+        int arr[26]; // tricky
+        arr['I' - 'A'] = 1;
+        arr['V' - 'A'] = 5;
+        arr['X' - 'A'] = 10;
+        arr['L' - 'A'] = 50;
+        arr['C' - 'A'] = 100;
+        arr['D' - 'A'] = 500;
+        arr['M' - 'A'] = 1000;
+        
+        int ret = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (i + 1 < s.size() && arr[s[i]-'A'] < arr[s[i + 1]-'A'])
+                ret += arr[s[i + 1]-'A'] - arr[s[i++]-'A'];
+            else ret += arr[s[i]-'A'];
         }
         return ret;
     }
