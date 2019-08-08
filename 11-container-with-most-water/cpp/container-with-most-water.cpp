a422902ca556d7275d8d1961e61b5fd293f6759e
zhaoyin666 <joy_yin@outlook.com>
2019-08-08 10:51:45 +0800

    container with most water

---
+++ 11-container-with-most-water/cpp/container-with-most-water.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ret = INT_MIN;
+        
+        for (int i = 0, j = height.size() - 1; i < j; height[i] > height[j] ? --j : ++i) {
+            ret = max( (j - i) * min(height[i], height[j]), ret );
+        }
+        return ret;
+    }
+};
