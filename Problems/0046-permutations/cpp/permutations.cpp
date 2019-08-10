c863da5ae15eebc6ee5b71df3da25c6cbe923da9
zhaoyin666 <joy_yin@outlook.com>
2019-08-10 13:57:38 +0800

    permutations 1

---
+++ Problems/0046-permutations/cpp/permutations.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+// 12 ms, 10.6 MB. Backtracking
+class Solution {
+private:
+    vector<vector<int>> ret;
+    unordered_set<int> used;
+    void __permute (vector<int>& nums, int index, vector<int> vec) {
+        
+        if (index == nums.size()) {
+            ret.emplace_back(vec);
+            return;
+        }
+        
+        for (int i = 0; i < nums.size(); ++ i)
+            if ( !used.count(nums[i]) ) {
+                used.insert(nums[i]);
+                vec.emplace_back( nums[i] );
+                __permute(nums, index + 1, vec);
+                vec.pop_back();
+                used.erase(nums[i]);
+            }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        ret.clear();
+        if (nums.empty()) return ret;
+        
+        used.clear();
+        
+        vector<int> tmp;
+        tmp.clear();
+        __permute(nums, 0, tmp);
+        
+        return ret;
+    }
+};
