df5b3e09cbb350b648df5941a2bad6ec0bfbac7b
zhaoyin666 <joy_yin@outlook.com>
2019-08-14 18:56:40 +0800

    unique binary search trees

---
+++ Problems/0097_unique_binary_search_trees/cc/unique_binary_search_trees.cc
@@ -0,0 +1,26 @@
+#include <vector>
+using namespace std;
+
+// 4 ms, 8.3 MB
+class Solution {
+ public:
+  int numTrees(int n) {
+    memo_ = vector<int>(n + 1, 0);
+    memo_[0] = 1;
+    memo_[1] = 1;
+    return GetNumTrees(n);
+  }
+  
+ private:
+  vector<int> memo_;
+  
+  int GetNumTrees(const int& n) {
+    if (memo_[n] != 0) {
+      return memo_[n];
+    }
+    for (int i = 1; i <= n; ++i) {
+      memo_[n] += GetNumTrees(i - 1) * GetNumTrees(n - i);
+    }
+    return memo_[n];
+  }
+};
