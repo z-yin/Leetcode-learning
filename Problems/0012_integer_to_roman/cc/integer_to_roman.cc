6672ff37f1b7551a601816abd0de73ab0c0823c4
zhaoyin666 <joy_yin@outlook.com>
2019-08-25 08:19:51 +0800

    integer to roman

---
+++ Problems/0012_integer_to_roman/cc/integer_to_roman.cc
@@ -0,0 +1,23 @@
+#include <string>
+using namespace std;
+
+// 0 ms, 8.2 MB
+class Solution {
+ public:
+  string intToRoman(int num) {
+    char* mapping[13] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+    int base[13] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+    int base_index = 12;
+    string ret;
+    while (num != 0) {
+      int divide = num / base[base_index];
+      num %= base[base_index];
+      while (divide) {
+        ret += mapping[base_index];
+        --divide;
+      }
+      --base_index;
+    }
+    return ret;
+  }
+};
