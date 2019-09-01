7ffe71c501e882a97c84d151970925c3f844625b
joy <joy_yin@outlook.com>
2019-09-01 17:08:10 +0200

    different ways to add parentheses

--- Problems/0241_different_ways_to_add_parentheses/cc/different_ways_to_add_parentheses.cc
+++ Problems/0241_different_ways_to_add_parentheses/cc/different_ways_to_add_parentheses.cc
@@ -1,6 +1,6 @@
-#include <vector>
-#include <string>
 #include <map>
+#include <string>
+#include <vector>
 using namespace std;
 
 // 0 ms, 9.4 MB. Divide and conquer. Memoization.
@@ -11,28 +11,26 @@ class Solution {
       return memo_[input];
     }
     vector<int> ret;
-    if (input.empty())
-      return ret;
-    
+    if (input.empty()) return ret;
+
     for (int i = 0; i < input.size(); ++i) {
       char c = input[i];
-      if (isdigit(c))
-        continue;
-      
+      if (isdigit(c)) continue;
+
       vector<int> l = diffWaysToCompute(input.substr(0, i));
-      vector<int> r = diffWaysToCompute(input.substr(i + 1, input.size() - i - 1));
+      vector<int> r =
+          diffWaysToCompute(input.substr(i + 1, input.size() - i - 1));
       for (int le : l) {
         for (int re : r) {
           ret.emplace_back(calc(le, re, c));
         }
       }
     }
-    if (ret.empty())
-      ret.emplace_back(stoi(input));
+    if (ret.empty()) ret.emplace_back(stoi(input));
     memo_[input] = ret;
     return ret;
   }
-  
+
  private:
   map<string, vector<int>> memo_;
   int calc(int lh, int rh, char opt) {
