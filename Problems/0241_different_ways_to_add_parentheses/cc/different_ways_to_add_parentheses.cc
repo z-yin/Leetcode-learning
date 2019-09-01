02f207c3320f77353ce2fe7f594d266393d40acd
joy <joy_yin@outlook.com>
2019-09-01 11:06:37 +0200

    different ways to add parentheses

---
+++ Problems/0241_different_ways_to_add_parentheses/cc/different_ways_to_add_parentheses.cc
@@ -0,0 +1,56 @@
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+
+// 0 ms, 9.4 MB. Divide and conquer. Memoization.
+class Solution {
+ public:
+  vector<int> diffWaysToCompute(string input) {
+    if (memo_.count(input)) {
+      return memo_[input];
+    }
+    vector<int> ret;
+    if (input.empty())
+      return ret;
+    
+    for (int i = 0; i < input.size(); ++i) {
+      char c = input[i];
+      if (isdigit(c))
+        continue;
+      
+      vector<int> l = diffWaysToCompute(input.substr(0, i));
+      vector<int> r = diffWaysToCompute(input.substr(i + 1, input.size() - i - 1));
+      for (int le : l) {
+        for (int re : r) {
+          ret.emplace_back(calc(le, re, c));
+        }
+      }
+    }
+    if (ret.empty())
+      ret.emplace_back(stoi(input));
+    memo_[input] = ret;
+    return ret;
+  }
+  
+ private:
+  map<string, vector<int>> memo_;
+  int calc(int lh, int rh, char opt) {
+    int res;
+    switch (opt) {
+      case '+': {
+        res = lh + rh;
+        break;
+      }
+      case '-': {
+        res = lh - rh;
+        break;
+      }
+      case '*': {
+        res = lh * rh;
+        break;
+      }
+    }
+    return res;
+  }
+};
