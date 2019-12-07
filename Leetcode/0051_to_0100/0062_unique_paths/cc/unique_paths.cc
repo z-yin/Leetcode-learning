dc3306f86cb26279be423e36ff7feb9bede99993
joy <joy_yin@outlook.com>
2019-12-07 13:54:01 +0100

    unique paths

--- Leetcode/0051_to_0100/0062_unique_paths/cc/unique_paths.cc
+++ Leetcode/0051_to_0100/0062_unique_paths/cc/unique_paths.cc
@@ -1,28 +1,18 @@
 #include <vector>
 using namespace std;
 
-// 4 ms, 8.8 MB. DP
+// 0 ms	8.6 MB. DP
 class Solution {
  public:
   int uniquePaths(int m, int n) {
-    memo_ = vector<vector<int>>(n, vector<int>(m, -1));
-    for (int i = 1; i < n; ++i) {
-      memo_[i][0] = 1;
+    vector<vector<int>> mat(m, vector<int>(n, 0));
+    for (int i = 0; i < m; ++i) mat[i][0] = 1;
+    for (int i = 0; i < n; ++i) mat[0][i] = 1;
+    for (int i = 1; i < m; ++i) {
+      for (int j = 1; j < n; ++j) {
+        mat[i][j] = mat[i - 1][j] + mat[i][j - 1];
+      }
     }
-    for (int i = 0; i < m; ++i) {
-      memo_[0][i] = 1;
-    }
-    return GetUniquePaths(m - 1, n - 1);
-  }
-
- private:
-  vector<vector<int>> memo_;
-
-  int GetUniquePaths(const int& m, const int& n) {
-    if (memo_[n][m] != -1) {
-      return memo_[n][m];
-    }
-    memo_[n][m] = GetUniquePaths(m - 1, n) + GetUniquePaths(m, n - 1);
-    return memo_[n][m];
+    return mat[m - 1][n - 1];
   }
 };
