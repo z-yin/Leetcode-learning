3b411e260c728a72ee6730dc5ea0de5f85cd7eef
joy <joy_yin@outlook.com>
2019-12-07 14:00:09 +0100

    unique paths

--- Leetcode/0051_to_0100/0062_unique_paths/cc/unique_paths.cc
+++ Leetcode/0051_to_0100/0062_unique_paths/cc/unique_paths.cc
@@ -5,14 +5,14 @@ using namespace std;
 class Solution {
  public:
   int uniquePaths(int m, int n) {
-    vector<vector<int>> mat(m, vector<int>(n, 0));
-    for (int i = 0; i < m; ++i) mat[i][0] = 1;
+    vector<vector<int>> mat(2, vector<int>(n, 0));
+    for (int i = 1; i < 2; ++i) mat[i][0] = 1;
     for (int i = 0; i < n; ++i) mat[0][i] = 1;
     for (int i = 1; i < m; ++i) {
       for (int j = 1; j < n; ++j) {
-        mat[i][j] = mat[i - 1][j] + mat[i][j - 1];
+        mat[i % 2][j] = mat[(i - 1) % 2][j] + mat[i % 2][j - 1];
       }
     }
-    return mat[m - 1][n - 1];
+    return mat[(m - 1) % 2][n - 1];
   }
 };
