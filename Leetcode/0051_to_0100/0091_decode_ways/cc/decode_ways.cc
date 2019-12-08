1d6ce5a9c044684afde9a72c26bfbe6ab4e9a0a0
joy <joy_yin@outlook.com>
2019-12-08 20:11:11 +0100

    decode ways

--- Leetcode/0051_to_0100/0091_decode_ways/cc/decode_ways.cc
+++ Leetcode/0051_to_0100/0091_decode_ways/cc/decode_ways.cc
@@ -1,27 +1,22 @@
 #include <string>
 using namespace std;
 
-// 4ms, 8,5 MB. DP
+// 0 ms	8.3 MB. DP. Space O(3).
 class Solution {
  public:
   int numDecodings(string s) {
-    int size = s.size();
-    if (size == 0 || s[0] == '0') {
+    if (s[0] == '0') 
       return 0;
-    }
-    int *memo = new int[size + 1];
-    memo[0] = 1;
-    memo[1] = 1;
-
-    for (int i = 2; i <= size; ++i) {
-      memo[i] = 0;
-      if (s[i - 1] != '0') {
-        memo[i] = memo[i - 1];
-      }
-      if (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] < '7')) {
-        memo[i] += memo[i - 2];
+    int f[3] = {0};
+    f[0] = f[1] = 1;
+    for (int i = 2; i <= s.size(); ++i) {
+      f[i % 3] = 0; // reset
+      if (s[i - 1] - '0' != 0) f[i % 3] = f[(i - 1) % 3];
+      if (s[i - 2] - '0' == 1
+          || (s[i - 2] - '0' == 2 && s[i - 1] - '0' <= 6)) {
+        f[i % 3] += f[(i - 2) % 3];
       }
     }
-    return memo[size];
+    return f[s.size() % 3];
   }
 };
