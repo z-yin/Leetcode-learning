f199f0d7650242bcb3b9186724a37cd353521136
joy <joy_yin@outlook.com>
2019-12-11 14:02:37 +0100

    word break solution 2

--- Leetcode/0101_to_0150/0139_word_break/cc/word_break.cc
+++ Leetcode/0101_to_0150/0139_word_break/cc/word_break.cc
@@ -3,7 +3,7 @@
 using namespace std;
 
 // 4 ms, 8.8 MB. DP
-class Solution {
+class Solution1 {
  public:
   bool wordBreak(string s, vector<string>& wordDict) {
     can_ = vector<int>(s.size() + 1, -1);
@@ -32,5 +32,31 @@ class Solution {
       can_[end] = 0;
     }
     return can_[end];
+  }
+};
+
+// 0 ms	8.6 MB. DP.
+class Solution2 {
+ public:
+  bool wordBreak(string s, vector<string>& wordDict) {
+    vector<bool> dp(s.size() + 1, false);
+    dp[0] = true;
+
+    for (int i = 1; i < s.size() + 1; ++i) {
+      for (auto& w : wordDict) {
+        if (w.size() <= i && match(s, i - w.size(), i - 1, w) &&
+            dp[i - w.size()]) {
+          dp[i] = true;
+          break;
+        }
+      }
+    }
+    return dp[s.size()];
+  }
+
+  bool match(string& s, int i, int j, string& t) {
+    for (int k = 0; i <= j; ++k, ++i)
+      if (t[k] != s[i]) return false;
+    return true;
   }
 };
