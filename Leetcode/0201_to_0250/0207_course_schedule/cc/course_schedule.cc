62957fe6b685c9892de9ee2a8429cc5afd396ea7
joy <joy_yin@outlook.com>
2019-12-13 23:09:46 +0100

    course schedule: BFS and DFS

--- Leetcode/0201_to_0250/0207_course_schedule/cc/course_schedule.cc
+++ Leetcode/0201_to_0250/0207_course_schedule/cc/course_schedule.cc
@@ -1,9 +1,10 @@
 #include <queue>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 
-// bfs
-class Solution {
+// 44 ms	11.9 MB. BFS.
+class Solution1 {
  public:
   bool canFinish(int n, vector<vector<int>> &pre) {
     if (n == 0) return false;
@@ -31,5 +32,42 @@ class Solution {
       }
     }
     return num_free == n;
+  }
+};
+
+// 16 ms	14.6 MB. DFS.
+class Solution2 {
+ public:
+  bool canFinish(int n, vector<vector<int>> &pre) {
+    if (n == 0) return false;
+    if (pre.empty()) return true;
+
+    unordered_map<int, vector<int>> mp;
+    for (auto &v : pre) {
+      if (mp.count(v[1])) {
+        mp[v[1]].emplace_back(v[0]);
+      } else {
+        mp[v[1]] = vector<int>{v[0]};
+      }
+    }
+
+    vector<int> visit(n, 0);
+    for (int i = 0; i < n; ++i)
+      if (!dfs(mp, visit, i)) return false;
+
+    return true;
+  }
+
+  bool dfs(unordered_map<int, vector<int>> &mp, vector<int> &v, int i) {
+    if (v[i] == -1) return false;
+    if (v[i] == 1) return true;
+
+    v[i] = -1;
+    if (mp.count(i))
+      for (int c : mp[i])
+        if (!dfs(mp, v, c)) return false;
+
+    v[i] = 1;
+    return true;
   }
 };
