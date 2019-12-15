824da2819c08c4789f0a2f26b13dcd2994590910
joy <joy_yin@outlook.com>
2019-12-15 19:49:33 +0100

    find the duplicate number

---
+++ Leetcode/0251_to_0300/0287_find_the_duplicate_number/find_the_duplicate_number.cc
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+// 12 ms	9.9 MB. Floyd's Tortoise and Hare.
+class Solution {
+ public:
+  int findDuplicate(vector<int>& nums) {
+    int slow = nums[0], fast = nums[0];
+    do {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+    } while (slow != fast);
+    slow = nums[0];
+    while (slow != fast) {
+      slow = nums[slow];
+      fast = nums[fast];
+    }
+    return slow;
+  }
+};
