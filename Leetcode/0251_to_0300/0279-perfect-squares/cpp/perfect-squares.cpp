e122f865319739ed2bf53ff56972eb44d21bcb6b
joy <joy_yin@outlook.com>
2019-12-15 19:13:50 +0100

    perfect squares: Lagrange’s Four-Square Theorem

--- Leetcode/0251_to_0300/0279-perfect-squares/cpp/perfect-squares.cpp
+++ Leetcode/0251_to_0300/0279-perfect-squares/cpp/perfect-squares.cpp
@@ -1,21 +1,42 @@
 #include <algorithm>
+#include <climits>
+#include <cmath>
+#include <vector>
 using namespace std;
 
-// 104 ms, 11.3 MB
-class Solution {
-public:
-    int numSquares(int n) {
-        int *memo = new int[n + 1];
-        memo[0] = 0;
-        for (int i = 1; i < n + 1; ++ i)
-            memo[i] = INT_MAX;
-        
-        for (int i = 1; i < n + 1; ++ i) {
-            for (int sqr = 1, mx = sqrt(i); sqr <= mx; ++ sqr) {
-                memo[i] = min(memo[i], 1 + memo[i - sqr*sqr]);
-            }
-        }
-        return memo[n];
+// 100 ms	11.4 MB. DP.
+class Solution1 {
+ public:
+  int numSquares(int n) {
+    vector<int> dp(n + 1, INT_MAX);
+    dp[0] = 0;
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 1; j * j <= i; ++j) {
+        dp[i] = min(dp[i], dp[i - j * j] + 1);
+      }
     }
+    return dp[n];
+  }
 };
 
+// 0 ms	8.3 MB. Lagrange’s Four-Square Theorem.
+class Solution2 {
+ public:
+  bool isSquare(int n) {
+    int sqrt_n = sqrt(n);
+    return n == (sqrt_n * sqrt_n);
+  }
+
+  int numSquares(int n) {
+    if (isSquare(n)) return 1;
+
+    while ((n & 3) == 0) n >>= 2;
+    if ((n & 7) == 7) return 4;
+
+    int i = sqrt(n);
+    for (int j = 0; j <= i; ++j)
+      if (isSquare(n - j * j)) return 2;
+
+    return 3;
+  }
+};
